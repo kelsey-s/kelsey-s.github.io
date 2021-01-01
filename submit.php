05dec91780e25e1928d96233de5ff47b1fbba13b
kelsey-s <58612892+kelsey-s@users.noreply.github.com>
2020-12-31 19:27:10 -0500

    Update submit.php

--- submit.php
+++ submit.php
@@ -3,17 +3,12 @@
     {
         $name = $_POST['name']; // Get Name value from HTML Form
         $email_id = $_POST['email']; // Get Email Value
-        $msg = $_POST['message']; // Get Message Value
-         
+        $msg = $_POST['message']; // Get Message Value 
         $to = "admin@kelseysyshi.com"; // You can change here your Email
         $subject = "Website Message from '$name' "; // This is your subject
-          
-        // Always set content-type when sending HTML email
-        $header = "From: '$email_id'";
- 
+
         // More headers
-        mail($to, $subject, $msg, $header);
-    
+        mail($to, $subject, $msg, "From:" . $email_id);
+        
         header('Location: http://www.kelseysyshi.com/success.html');
     }
-?>
