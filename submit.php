8106c2f73e1a3d364c623485447d8b5a811212f7
Shiyu Shi <shiyu@Shiyus-MacBook-Pro.local>
2020-12-31 19:01:52 -0500

    Initial commit

--- submit.php
+++ submit.php
@@ -5,40 +5,15 @@
         $email_id = $_POST['email']; // Get Email Value
         $msg = $_POST['message']; // Get Message Value
          
-        $to = "shiyu.shi@marylandsmith.umd.edu"; // You can change here your Email
+        $to = "admin@kelseysyshi.com"; // You can change here your Email
         $subject = "Website Message from '$name' "; // This is your subject
-         
-        // HTML Message Starts here
-        $message ="
-        <html>
-            <body>
-                <table style='width:600px;'>
-                    <tbody>
-                        <tr>
-                            <td style='width:150px'><strong>Name: </strong></td>
-                            <td style='width:400px'>$name</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Email ID: </strong></td>
-                            <td style='width:400px'>$email_id</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Message: </strong></td>
-                            <td style='width:400px'>$msg</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </body>
-        </html>
-        ";
-         
+          
         // Always set content-type when sending HTML email
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        $header = "From: '$email_id'";
  
         // More headers
-        mail($admin_email, "New Form Submission", $message . ' - ' . $phone, "From:" . $email);
+        mail($to, $subject, $msg, $header);
     
-        header('Location: http://127.0.0.1:5500/dist/success.html');
+        header('Location: http://www.kelseysyshi.com/success.html');
     }
 ?>
